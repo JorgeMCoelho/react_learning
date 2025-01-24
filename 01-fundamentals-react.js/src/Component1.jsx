2ffd9c1bec953dbb6300c525b61970676f9e1fb9
JorgeMCoelho <draculshadow@gmail.com>
2025-01-24 17:01:01 +0000

    Third baby steps - Properties

--- 01-fundamentals-react.js/src/Component1.jsx
+++
@@ -1,3 +0,0 @@
-export function Componente1(){
-    return <p>This is a component</p>
-}
--- 01-fundamentals-react.js/src/main.jsx
+++ 01-fundamentals-react.js/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.jsx'
-import { Componente1 } from './Component1.jsx'
+import { Post } from './Post.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Componente1 />
+    <Post 
+      author="Jorge Coelho"
+      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, nesciunt ea quis optio deserunt nam. Nobis hic sed voluptatem impedit, quaerat ipsum et nesciunt odio amet eaque, pariatur laboriosam recusandae."  
+    />
   </StrictMode>,
 )
