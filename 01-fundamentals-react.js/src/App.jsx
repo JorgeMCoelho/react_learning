957d56286c8a305e4409b1aa410bfd038ff2112e
JorgeMCoelho <draculshadow@gmail.com>
2025-01-24 17:44:51 +0000

    Forth baby step - CSS

--- 01-fundamentals-react.js/src/App.jsx
+++ 01-fundamentals-react.js/src/App.jsx
@@ -1,8 +1,22 @@
-export function App() {
+import { Header } from "./components/Header";
+import { Post } from "./Post";
 
+import './styles.css'
+
+export function App() {
   return (
-    <>
+    <div>
+      <Header />
       <h1>Hello World :D</h1>
-    </>
+      <Post
+        author="Jorge Coelho"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, nesciunt ea quis optio deserunt nam. Nobis hic sed voluptatem impedit, quaerat ipsum et nesciunt odio amet eaque, pariatur laboriosam recusandae."
+      />
+      <Post 
+        author="António Costa"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, nesciunt ea quis optio deserunt nam. Nobis hic sed voluptatem impedit, quaerat ipsum et nesciunt odio amet eaque, pariatur laboriosam recusandae."
+      />
+    </div>
+
   )
 }
--- 01-fundamentals-react.js/src/main.jsx
+++ 01-fundamentals-react.js/src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App.jsx'
 import { Post } from './Post.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+
     <App />
-    <Post 
-      author="Jorge Coelho"
-      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, nesciunt ea quis optio deserunt nam. Nobis hic sed voluptatem impedit, quaerat ipsum et nesciunt odio amet eaque, pariatur laboriosam recusandae."  
-    />
+
   </StrictMode>,
 )
