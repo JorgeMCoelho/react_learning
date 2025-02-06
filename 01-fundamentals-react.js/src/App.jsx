043bf2bc9f0e72921b6da27b5bd877167127a035
JorgeMCoelho <draculshadow@gmail.com>
2025-02-06 16:49:49 +0000

    Added sidebar

--- 01-fundamentals-react.js/src/App.jsx
+++ 01-fundamentals-react.js/src/App.jsx
@@ -2,20 +2,28 @@ import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 import './global.css'
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <h1>Hello World :D</h1>
-      <Post
-        author="Jorge Coelho"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, nesciunt ea quis optio deserunt nam. Nobis hic sed voluptatem impedit, quaerat ipsum et nesciunt odio amet eaque, pariatur laboriosam recusandae."
-      />
-      <Post 
-        author="António Costa"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, nesciunt ea quis optio deserunt nam. Nobis hic sed voluptatem impedit, quaerat ipsum et nesciunt odio amet eaque, pariatur laboriosam recusandae."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+          author="Jorge Coelho"
+          content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, nesciunt ea quis optio deserunt nam. Nobis hic sed voluptatem impedit, quaerat ipsum et nesciunt odio amet eaque, pariatur laboriosam recusandae."
+          />
+          <Post 
+            author="António Costa"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, nesciunt ea quis optio deserunt nam. Nobis hic sed voluptatem impedit, quaerat ipsum et nesciunt odio amet eaque, pariatur laboriosam recusandae."
+          />
+        </main>
+      </div>
+
+      
     </div>
 
   )
