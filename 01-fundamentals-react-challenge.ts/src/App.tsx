0b6c7ac971bd84cdd00f664e13875ae98497a1be
JorgeMCoelho <draculshadow@gmail.com>
2025-02-23 11:38:07 +0000

    fixed responsive behaviour

--- 01-fundamentals-react-challenge.ts/src/App.tsx
+++ 01-fundamentals-react-challenge.ts/src/App.tsx
@@ -2,13 +2,16 @@ import { Header } from './components/Header';
 import { TaskManager } from './components/TaskManager';
 
 import './global.css';
+import styles from './App.module.css'
 
 export function App() {
 
   return (
     <div>
       <Header />
-      <TaskManager />
+      <div className={styles.wrapper}>
+        <TaskManager />
+      </div>
     </div>
   )
 }
