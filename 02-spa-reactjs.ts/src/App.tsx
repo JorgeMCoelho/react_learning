91d9d376ca1e80dd7531b33680a4714080479ba3
JorgeMCoelho <draculshadow@gmail.com>
2025-02-27 16:23:10 +0000

    Header, Layout & NavLink

--- 02-spa-reactjs.ts/src/App.tsx
+++ 02-spa-reactjs.ts/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 
 import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/themes/global'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
