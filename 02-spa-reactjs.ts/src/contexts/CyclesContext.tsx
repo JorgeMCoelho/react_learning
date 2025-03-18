b9a247ce7ba3a4698448c01beab72af077106c34
JorgeMCoelho <draculshadow@gmail.com>
2025-03-18 14:59:21 +0000

    Second class of Reducers

--- 02-spa-reactjs.ts/src/contexts/CyclesContext.tsx
+++ 02-spa-reactjs.ts/src/contexts/CyclesContext.tsx
@@ -33,23 +33,47 @@ interface CyclesContextProviderProps {
   children: ReactNode
 }
 
+interface CyclesState {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cycles, dispatch] = useReducer((state: Cycle[], action: any) => {
+  const [cyclesState, dispatch] = useReducer((state: CyclesState, action: any) => {
     
     if (action.type === 'ADD_NEW_CYCLE') {
-      return [...state, action.payload.newCycle]
+      return {
+        ...state,
+        cycles: [...state.cycles, action.payload.newCycle],
+        activeCycleId: action.payload.newCycle.id,
+      }
+    }
+
+    if (action.type === 'INTERRUPT_CURRENT_CYCLE'){
+      return{
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null,
+      }
     }
 
     return state
-  }, [])
+  }, {
+    cycles:[],
+    activeCycleId: null
+  })
 
-
-
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
+  const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
   function setSecondsPassed(seconds: number) {
@@ -95,7 +119,7 @@ export function CyclesContextProvider({
     })
 
     //setCycles((state) => [...state, newCyle])
-    setActiveCycleId(id)
+
     setAmountSecondsPassed(0)
   }
 
@@ -117,7 +141,6 @@ export function CyclesContextProvider({
         }
       }),
     ) */
-    setActiveCycleId(null)
   }
 
   return (
