d3bb0ea2bc66b86cb7c2c999d633b71aa13fcfd1
JorgeMCoelho <draculshadow@gmail.com>
2025-03-28 17:06:09 +0000

    Changed table placement to the correct container

--- 02-spa-reactjs-challenge.ts/src/pages/Home/index.tsx
+++ 02-spa-reactjs-challenge.ts/src/pages/Home/index.tsx
@@ -11,19 +11,20 @@ export function Home() {
                 <IntroContainerText>
                     <Text variant="titleXl">Find the perfect coffee for any time of the day.</Text>
                     <Text variant="textL">With Coffee Delivery, you receive your coffee wherever you are, at any time.</Text>
+                    <table>
+                        <tr>
+                            <td><ShoppingCart size={32} weight="fill" />Simple and secure purchase</td>
+                            <td><Package size ={32} weight="fill" />The packaging keeps the coffee intact</td>
+                        </tr>
+                        <tr>
+                            <td><Timer size={32} weight="fill" />Fast and tracked delivery</td>
+                            <td><Coffee size={32} weight="fill" />The coffee arrives fresh at your doorstep</td>
+                        </tr>
+                    </table>
                 </IntroContainerText>
                 <img src={coffeeDelivery} alt="" />
             </IntroContainer>
-            <table>
-                <tr>
-                    <td><ShoppingCart size={32} weight="fill" />Simple and secure purchase</td>
-                    <td><Package size ={32} weight="fill" />The packaging keeps the coffee intact</td>
-                </tr>
-                <tr>
-                    <td><Timer size={32} weight="fill" />Fast and tracked delivery</td>
-                    <td><Coffee size={32} weight="fill" />The coffee arrives fresh at your doorstep</td>
-                </tr>
-            </table>
+
         </HomeContainer>
     )
 }
