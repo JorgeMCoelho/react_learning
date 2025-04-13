e58ce25b4373cb1275100da6de0fd345fe39aed8
JorgeMCoelho <draculshadow@gmail.com>
2025-04-13 10:41:12 +0000

    Continuing to dev Checkout page CSS

--- 02-spa-reactjs-challenge.ts/src/pages/Checkout/index.tsx
+++ 02-spa-reactjs-challenge.ts/src/pages/Checkout/index.tsx
@@ -1,60 +1,67 @@
-import { AdressInfoContainer, CheckoutCompleteOrderContainer, CheckoutContainer } from "./styles";
+import { AdressInfoContainer, CheckoutCompleteOrderContainer, CheckoutWrapper, PaymentInfoContainer, SelectedCoffeesContainer } from "./styles";
+
+import { Bank, CreditCard, CurrencyEur, MapPinLine, Money } from "phosphor-react"
 
 
 export function Checkout(){
     return(
-        <CheckoutContainer>
-            <CheckoutCompleteOrderContainer>
+        <CheckoutWrapper>
+            <div>
                 <h1>Complete your Order</h1>
-                <AdressInfoContainer>
-                    <h1>Delivery Address</h1>
-                    <h3>Enter the address where you want to receive your order</h3>
-                    <form action="">
-                        <input type="text" placeholder="Zip Code" />
-                        <input type="text" placeholder="Street" />
-                        <input type="text" placeholder="Number" />
-                        <input type="text" placeholder="Aditional Info" /> 
-                        <input type="text" placeholder="Neighborhood" />
-                        <input type="text" placeholder="City" />
-                        <input type="text" placeholder="State" />
-                    </form>
-                    <div>
-                        <h1>Payment</h1>
+                <CheckoutCompleteOrderContainer>
+
+                    <AdressInfoContainer>
+                        <h1><MapPinLine size={22} /> Delivery Address</h1>
+                        <h3>Enter the address where you want to receive your order</h3>
+                        <form action="">
+                            <input type="text" placeholder="Zip Code" />
+                            <input type="text" placeholder="Street" />
+                            <input type="text" placeholder="Number" />
+                            <input type="text" placeholder="Aditional Info" /> 
+                            <input type="text" placeholder="Neighborhood" />
+                            <input type="text" placeholder="City" />
+                            <input type="text" placeholder="State" />
+                        </form>
+                    </AdressInfoContainer>
+                    <PaymentInfoContainer>
+                        <h1><CurrencyEur size={22} />Payment</h1>
                         <h3>Payment is made upon delivery. Choose the payment method you prefer</h3>
                         <form action="">
                             <label>
                                 <input type="radio" name="paymentMethod" value="credit" />
-                                <span>Credit Card</span>
+                                <span><CreditCard size={16} /> Credit Card</span>
                             </label>
                             <label>
                                 <input type="radio" name="paymentMethod" value="debit" />
-                                <span>Debit Card</span>
+                                <span><Bank size={16} />Debit Card</span>
                             </label>
                             <label>
                                 <input type="radio" name="paymentMethod" value="money" />
-                                <span>Money</span>
+                                <span><Money size={16} />Money</span>
                             </label>
                         </form>
-                    </div>
-                </AdressInfoContainer>
-            </CheckoutCompleteOrderContainer>
+                    </PaymentInfoContainer>
+                </CheckoutCompleteOrderContainer>
+            </div>
             <div>
                  <h1>Selected Coffees</h1>
-                 <h1>Coffee Card here</h1>
-                 <div>
-                    <h3>Subtotal</h3>
-                    <h3>29,00€</h3>
-                 </div>
-                 <div>
-                    <h3>Delivery fee</h3>
-                    <h3>3,00€</h3>
-                 </div>
-                 <div>
-                    <h3>Order Total</h3>
-                    <h3>32,00€</h3>
-                    <button>Submit Order</button>
-                 </div>
+                 <SelectedCoffeesContainer>
+                    <h1>Coffee Card here</h1>
+                    <div>
+                        <h3>Subtotal</h3>
+                        <h3>29,00€</h3>
+                    </div>
+                    <div>
+                        <h3>Delivery fee</h3>
+                        <h3>3,00€</h3>
+                    </div>
+                    <div>
+                        <h3>Order Total</h3>
+                        <h3>32,00€</h3>
+                        <button>Submit Order</button>
+                    </div>
+                 </SelectedCoffeesContainer>
             </div>
-        </CheckoutContainer>
+        </CheckoutWrapper>
     )
 }
--- 02-spa-reactjs-challenge.ts/src/pages/Checkout/styles.ts
+++ 02-spa-reactjs-challenge.ts/src/pages/Checkout/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const CheckoutContainer = styled.div`
+export const CheckoutWrapper = styled.div`
     display: grid;
     grid-template-columns: 1fr 28rem;
     max-width: 80rem;
@@ -11,8 +11,56 @@ export const CheckoutContainer = styled.div`
 export const CheckoutCompleteOrderContainer = styled.div`
     display: flex;
     flex-direction: column;
+    gap:0.9375rem;
+`
+export const CheckoutContainers = styled.div`
+    background-color: ${props => props.theme.colors['base-card']};
+    padding: 2.5rem;
+    border-radius: 6px;
 `
 
-export const AdressInfoContainer = styled.div`
+export const AdressInfoContainer = styled(CheckoutContainers)`
+
+`
+
+export const PaymentInfoContainer = styled(CheckoutContainers)`
+    form {
+        display: flex;
+        gap: 0.75rem;
+        margin-top: 2rem;
+    }    
+
+    label {
+
+        width: 100%;
+        padding: 1rem auto;
+        gap: 0.5rem;        
+
+        background-color: ${props => props.theme.colors['base-button']};
+
+        border-radius: 6px;
+        cursor: pointer;
+        transition: 0.2s all;
+
+        &:hover{
+            background-color: ${props => props.theme.colors['base-hover']};
+        }
+        span{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 0.75rem;
+        }
+    }
+
+    input[type="radio"] {  
+        display: none;    
+    }     
+`
+
+export const SelectedCoffeesContainer = styled.div`
     background-color: ${props => props.theme.colors['base-card']};
+    padding: 2.5rem;
+    border-radius: 6px 44px 6px 44px;
+    
 `
