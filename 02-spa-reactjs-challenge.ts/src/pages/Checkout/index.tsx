2f806b901f0fc590e785a58e42800609dd536f96
JorgeMCoelho <draculshadow@gmail.com>
2025-04-25 12:47:09 +0000

    Prepared the page Success to be built

--- 02-spa-reactjs-challenge.ts/src/pages/Checkout/index.tsx
+++ 02-spa-reactjs-challenge.ts/src/pages/Checkout/index.tsx
@@ -22,7 +22,7 @@ export function Checkout(){
         {
             name: 'Money',
             icon: <Money size={16} />,
-            isSelected: true,
+            isSelected: false,
         }
     ])
 
